0838b912b362c42ec70b6b8ed97e58201aa0e416
earth774 <sutthiphongnuanma@gmail.com>
2023-11-12 06:58:12 +0700

    Persistent Layouts

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,15 +1,14 @@
 <template>
-  <Layout>
-    <h1 class="text-3xl">
-      Home
-    </h1>
-  </Layout>
+  <h1 class="text-3xl">
+    Home
+  </h1>
 </template>
 
 <script>
 import Layout from "../Shared/Layout.vue";
 
 export default {
-  components: { Layout },
+  // components: { Layout },
+  layout: Layout,
 };
 </script>
