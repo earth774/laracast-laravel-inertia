6bf9eaf5e7ddee99b96f6d44012ea5d7bf518b2c
earth774 <sutthiphongnuanma@gmail.com>
2023-11-12 06:54:10 +0700

    global component

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,5 @@
 import { createApp, h } from 'vue'
-import { createInertiaApp } from '@inertiajs/vue3'
-import { InertiaProgress } from '@inertiajs/progress';
+import { createInertiaApp,Link } from '@inertiajs/vue3'
 
 createInertiaApp({
   resolve: name => {
@@ -14,6 +13,7 @@ createInertiaApp({
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
+      .component("Link", Link)
       .mount(el)
   },
 }) 
