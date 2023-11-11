5806ed5465b311f918cd20b6788109c8978d28e2
earth774 <sutthiphongnuanma@gmail.com>
2023-11-11 14:09:07 +0700

    page and nav

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1>Home</h1>
+  
+    <p>In this series, we will use the following frameworks:</p>
+  
+    <ul>
+      <li v-for="framework of frameworks" v-text="framework"></li>
+    </ul>
+    <Nav />
+  </template>
+  
+  <script>
+import Nav from '../Shared/Nav.vue';
+  export default {
+    props: {
+      name: String,
+      frameworks: Array,
+    },
+    components: { Nav },
+  };
+  </script>
