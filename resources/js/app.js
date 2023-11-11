097b789f2f9c6c57ba64acec88fe44bd228f348d
earth774 <sutthiphongnuanma@gmail.com>
2023-11-11 14:40:07 +0700

    progress

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,14 +1,19 @@
 import { createApp, h } from 'vue'
 import { createInertiaApp } from '@inertiajs/vue3'
+import { InertiaProgress } from '@inertiajs/progress';
 
 createInertiaApp({
   resolve: name => {
     const pages = import.meta.glob('./Pages/**/*.vue', { eager: true })
     return pages[`./Pages/${name}.vue`]
   },
+  progress:{
+      color: 'red',
+      showSpinner: true,
+  },
   setup({ el, App, props, plugin }) {
     createApp({ render: () => h(App, props) })
       .use(plugin)
       .mount(el)
   },
-})
+}) 
